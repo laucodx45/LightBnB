ad72ace54e219747f40041ea0f3b8f682230d1af
Cody <codylau6233@hotmail.com>
2024-01-23 19:59:32 -0800

    Refactor 4_most_visited_cities.sql queries

--- 1_queries/4_most_visited_cities.sql
+++ 1_queries/4_most_visited_cities.sql
@@ -3,9 +3,3 @@ FROM reservations
 JOIN properties ON property_id = properties.id
 GROUP BY properties.city
 ORDER BY total_reservations DESC;
-
--- SELECT properties.city, COUNT(reservations.*) as total_reservations
--- FROM properties
--- LEFT JOIN reservations ON properties.id = property_id
--- GROUP BY properties.city
--- ORDER BY COUNT(reservations.*) DESC;
